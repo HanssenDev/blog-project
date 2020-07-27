7da6b4bd6672fbbb069e780dafa99f185f16d925
Hanssen <hramanjooloo@gmail.com>
2020-07-27 21:16:42 +0100

    add logic for compose page

--- app.js
+++ app.js
@@ -15,10 +15,37 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
+const posts = [];
 
 app.get("/", function(req, res) {
   res.render("home", {homeText: homeStartingContent});
+
+  console.log(posts);
+})
+
+app.get("/about", function(req, res) {
+  res.render("about", {aboutText: aboutContent});
+})
+
+app.get("/contact", function(req, res) {
+  res.render("contact", {contactText: contactContent});
+})
+
+app.get("/compose", function(req, res) {
+  res.render("compose");
+})
+
+
+app.post("/compose", function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/")
+
 })
 
 
